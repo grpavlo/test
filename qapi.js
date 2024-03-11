55a51fc602447f72c998fa40dfa5fdb8de67e596
grpavlo <97116462+grpavlo@users.noreply.github.com>
2024-03-11 15:57:25 +0200

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"65ef09758e2e8e0021797ec2",
+				"65ef09758e2e8e0021797ec5"
+			]
+		},
+		"65ef09758e2e8e0021797ec2": {
+			"id": "65ef09758e2e8e0021797ec2",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"65ef09758e2e8e0021797ec5": {
+			"id": "65ef09758e2e8e0021797ec5",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
